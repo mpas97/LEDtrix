f05e266637607a4359ba3149e153a362f6b555d2
Maurizio Pasquinelli <pasquinellimaurizio@gmail.com>
2019-01-23 13:17:44 +0100

    add chars to animation

--- animations.h
+++ animations.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "buttons.h"
 #include "leds.h"
+#include "chars.h"
 
 /**
  * Method for testing if x / y are displayed correct
@@ -11,6 +12,26 @@
  * @param leds the matrix
  */
 void demo() {
+    /*clear();
+    int character[5][5];
+    for (int c = 1; c <= 18;) {
+        for (int by = 0; by < 3; by++) {
+            for (int bx = 0; bx < 3; bx++) {
+                if (getChar(character, c)) {
+                    for (int y = 0; y < 5; y++) {
+                        for (int x = 0; x < 5; x++) {
+                            if (character[y][x]) leds[x + bx * 5][y + by * 5].blue = 255;
+                        }
+                    }
+                    updateMatrix();
+                    sleep(1);
+                    c++;
+                }
+            }
+        }
+        clear();
+    }*/
+
     clear();
     for (int y = 0; y < STRIP; y++) {
         for (int x = 0; x < STRIP; x++) {
