eb5d766812ed5b4365b147aff1bc4ee6c719e14b
Maurizio Pasquinelli <pasquinellimaurizio@gmail.com>
2019-01-25 15:29:58 +0100

    more animations

--- animations.h
+++ animations.h
@@ -6,6 +6,8 @@
 #include "leds.h"
 #include "chars.h"
 
+void drawSmile();
+
 /**
  * Method for testing if x / y are displayed correct
  *
@@ -76,15 +78,122 @@ void demo() {
         //fill Diagonal
         for (int i = 1; i < STRIP + 1; i++) {
             for (int j = 0; j < i; j++) {
-                leds[j][i - j].red = 105 - j;
-                leds[j][i - j].blue = 5 * j;
-                leds[j][i - j].green = 105 - i;
-                leds[STRIP - j][STRIP - i + j].red = 105 - j;
-                leds[STRIP - j][STRIP - i + j].blue = 5 * j;
-                leds[STRIP - j][STRIP - i + j].green = 105 - i;
+                leds[j][i - j-1].red = 105 - j;
+                leds[j][i - j-1].blue = 5 * j;
+                leds[j][i - j-1].green = 105 - i;
+                leds[STRIP-1 - j][STRIP - i + j].red = 105 - j;
+                leds[STRIP-1 - j][STRIP - i + j].blue = 5 * j;
+                leds[STRIP-1 - j][STRIP - i + j].green = 105 - i;
+                updateMatrix();
+
+            }
+        }
+        usleep(HALF_S_IN_U);
+        drawSmile();
+        usleep(HALF_S_IN_U);
+
+        for (int i = STRIP; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                leds[j][i - j-1].red = 60 - j;
+                leds[j][i - j-1].blue = 10 * j;
+                leds[j][i - j-1].green = 60 - i;
+                leds[STRIP-1 - j][STRIP - i + j].red = 60 - j;
+                leds[STRIP-1 - j][STRIP - i + j].blue = 10 * j;
+                leds[STRIP-1 - j][STRIP - i + j].green = 60 - i;
                 updateMatrix();
             }
         }
+        usleep(HALF_S_IN_U);
+
+        for (int i = STRIP; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                leds[STRIP - i + j][j].red = 150 - 3*j;
+                leds[STRIP - i + j][j].blue = 4 * j;
+                leds[STRIP - i + j][j].green = 70 - i;
+                leds[i - j-1][STRIP-1 - j].red = 150 - 3*j;
+                leds[i - j-1][STRIP-1 - j].blue = 4 * j;
+                leds[i - j-1][STRIP-1 - j].green = 70 - i;
+                updateMatrix();
+
+            }
+        }
+        for (int i = 1; i < STRIP + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                leds[i - j-1][j].red = 255 - 3*j;
+                leds[i - j-1][j].blue = 3 * j;
+                leds[i - j-1][j].green = 70 - i;
+                leds[STRIP - i + j][STRIP-1 - j].red = 255 - 3*j;
+                leds[STRIP - i + j][STRIP-1 - j].blue = 3 * j;
+                leds[STRIP - i + j][STRIP-1 - j].green = 70 - i;
+                updateMatrix();
+
+            }
+        }
+
+        //clear square middle to out
+        for(int j = STRIP/2; j >= 0; j--) {
+            for (int i = j; i < STRIP-j; i++) {
+                setLedsColor(j, i, WHITE);
+                setLedsColor(i, j, WHITE);
+                setLedsColor(STRIP-1-j, i, WHITE);
+                setLedsColor(i, STRIP -1- j, WHITE);
+            }
+            updateMatrix();
+            usleep(HALF_S_IN_U);
+        }
+
+        int red = 0;
+        int blue = 0;
+        int green = 255;
+
+        //fill square out to middle
+        for(int j = 0; j <= STRIP/2; j++) {
+            blue += 10;
+            green -= 2*j;
+            for (int i = j; i < STRIP-j; i++) {
+                leds[j][i].red = red;
+                leds[j][i].blue = blue;
+                leds[j][i].green = blue;
+                leds[i][j].red = red;
+                leds[i][j].blue = blue;
+                leds[i][j].green = green;
+                leds[STRIP - 1 - j][i].red = red;
+                leds[STRIP - 1 - j][i].blue = blue;
+                leds[STRIP - 1 - j][i].green = blue;
+                leds[i][STRIP - 1 - j].red = red;
+                leds[i][STRIP - 1 - j].blue = blue;
+                leds[i][STRIP - 1 - j].green = green;
+            }
+            updateMatrix();
+            usleep(HALF_S_IN_U);
+        }
+
+        blue = 0;
+        green = 255;
+
+        //fill square middle to out
+        for(int j = STRIP/2; j >= 0; j--) {
+            blue += 10;
+            green -= 2*j;
+            for (int i = j; i < STRIP-j; i++) {
+                leds[j][i].red = red;
+                leds[j][i].blue = blue;
+                leds[j][i].green = green;
+                leds[i][j].red = red;
+                leds[i][j].blue = blue;
+                leds[i][j].green = blue;
+                leds[STRIP - 1 - j][i].red = red;
+                leds[STRIP - 1 - j][i].blue = blue;
+                leds[STRIP - 1 - j][i].green = green;
+                leds[i][STRIP - 1 - j].red = red;
+                leds[i][STRIP - 1 - j].blue = blue;
+                leds[i][STRIP - 1 - j].green = blue;
+            }
+            updateMatrix();
+            usleep(HALF_S_IN_U);
+        }
+        usleep(HALF_S_IN_U);
+
     }
 
 }
@@ -118,6 +227,7 @@ void drawSmile() {
 
     }
 
+    //draw mouth
     for (int i = 0; i < 2; i++) {
         setLedsColor(4, 9 + i, col);
         setLedsColor(5, 9 + i, col);
@@ -128,7 +238,7 @@ void drawSmile() {
         setLedsColor(10, 9 + i, col);
     }
 
-    setLedsColor(6, 11, col);
+    setLedsColor(5, 11, col);
     setLedsColor(9, 11, col);
 
     updateMatrix();
